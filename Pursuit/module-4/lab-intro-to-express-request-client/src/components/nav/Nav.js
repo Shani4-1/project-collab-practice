e354b2893609119d79beb4a806996b7cdcfb5502
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-04-05 23:49:57 -0400

    Completed Nav component with Links.

--- Pursuit/module-4/lab-intro-to-express-request-client/src/components/nav/Nav.js
+++ Pursuit/module-4/lab-intro-to-express-request-client/src/components/nav/Nav.js
@@ -1,6 +1,27 @@
+import { Link } from "react-router-dom"
 import React from "react"
-import Home from "../home/Home"
+import "./components/nav/Nav.css"
+
 
 export default function Nav() {
-    return (<h1>Nav Bar</h1>)
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link className="nav-link fs-3" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link fs-3" to="/bugs">Bugs</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link fs-3" to="/:verb/:adjective/:noun">Words</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link fs-3" to="/pokemon-pretty">Pokemon</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
